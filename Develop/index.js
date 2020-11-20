02521c764e0931b760d2e8c46d9126269e49b044
Bert <robertgill82@gmail.com>
2020-11-20 15:55:15 -0700

    initial commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,13 @@
 // array of questions for user
 const questions = [
-
+  //
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
