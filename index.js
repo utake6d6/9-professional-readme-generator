98e3ab29c2092de85b0587c4c1ea3a032ecb982e
Bert <robertgill82@gmail.com>
2020-11-23 16:08:28 -0700

    SyntaxError: Unexpected string for license question

--- index.js
+++ index.js
@@ -17,64 +17,87 @@ const questions = [
     // Description
     type: "input",
     name: "description",
-    message: "",
+    message: "Provide a description of your project.",
   },
   {
     // Installation instructions
     type: "input",
     name: "installation",
-    message: "",
+    message:
+      "What are the steps required to get the development environment running?",
   },
   {
     // Usage information
     type: "input",
     name: "usage",
-    message: "",
+    message: "Provide instructions and examples for use.",
   },
   {
     // License
-    type: "input",
+    type: "list",
     name: "license",
-    message: "",
-    choices: [],
+    message: "Select a license for your project. (Required)",
+    choices: [
+      'MIT'
+      'Apache 2.0'
+      'ISC'
+      'N/A'
+    ],
+
+    validate: licenseList => {
+      if (licenseList) {
+        return true;
+      }
+      else {
+        console.log("Please select a listed license.");
+        return false;
+      }
+    },
   },
   {
     // Contribution guidelines
     type: "input",
     name: "contributing",
-    message: "",
+    message: "Provide some guidelines for how you'd like other developers to contribute to your project.",
   },
   {
     // Test instructions
     type: "input",
     name: "tests",
-    message: "",
-  },
-  {
-    // Questions
-    type: "input",
-    name: "questions",
-    message: "",
+    message: "Provide any tests for your application and examples on how to run them.",
   },
   {
     // GitHub username
     type: "input",
     name: "gitHubUsername",
-    message: "",
+    message: "Provide your GitHub username.",
   },
   {
     // Email address
     type: "input",
     name: "emailAddress",
-    message: "",
+    message: "Provide your GitHub affiliated email address.",
   },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("README.md generated");
+  });
+}
 
 // function to initialize program
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+  .then (answers => {
+    const generateDetails = generateMarkdown(answers);
+    writeToFile('./README.md', generateDetails);
+  });
+};
 
 // function call to initialize program
 init();
